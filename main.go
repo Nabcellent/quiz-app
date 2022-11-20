55d82af2989a811913838625d80690867cfebde2
Nabcellent <nabcellent.dev@gmail.com>
2022-11-20 14:17:02 +0300

    Completes project

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package quiz_app
+package main
 
 import (
 	"encoding/csv"
@@ -15,18 +15,25 @@ type Problem struct {
 
 func parseProblems(lines [][]string) []Problem {
 	//	Go over lines and parse them, with Problem struct
+	r := make([]Problem, len(lines))
+
+	for i := 0; i < len(lines); i++ {
+		r[i] = Problem{q: lines[i][0], a: lines[i][1]}
+	}
+
+	return r
 }
 
 func problemPuller(fileName string) ([]Problem, error) {
 	//	Read all problems from csv.
 
 	//	1.	Open file
-	if file, err := os.Open(fileName); err != nil {
+	if file, err := os.Open(fileName); err == nil {
 		//	2.	Create new reader
 		csvR := csv.NewReader(file)
 
 		//	3.	Read file
-		if cLines, err := csvR.ReadAll(); err != nil {
+		if cLines, err := csvR.ReadAll(); err == nil {
 
 			//	4.	Call problem parser func
 			return parseProblems(cLines), nil
@@ -55,14 +62,48 @@ func main() {
 	}
 
 	//	5.	Create a variable to count correct answers.
-	correctAnswer := 0
+	correctAns := 0
 
 	//	6.	Initialize timer using duration set in step 2.
 	tObj := time.NewTimer(time.Duration(*timer) * time.Second)
 	ansC := make(chan string)
 
 	//	7.	Loop through the problems, print the questions and accept answers.
+problemLoop:
+	for i, p := range problems {
+		var answer string
+		fmt.Printf("Problems %d: %s = ", i+1, p.q)
+
+		go func() {
+			fmt.Scanf("%s", &answer)
+
+			ansC <- answer
+		}()
+
+		select {
+		case <-tObj.C:
+			fmt.Println()
+			break problemLoop
+		case iAns := <-ansC:
+			if iAns == p.a {
+				correctAns++
+			}
+
+			if i == len(problems)-1 {
+				close(ansC)
+			}
+		}
+	}
 
 	//	8.	Calculate and print out result.
+	fmt.Printf("You result is %d out of %d\n", correctAns, len(problems))
+	fmt.Printf("Press enter to exit.")
 
+	<-ansC
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+
+	os.Exit(1)
 }
